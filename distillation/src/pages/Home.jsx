0301318e3ffa7043a9c76b0b19e570728f569a1e
yleeyilin <yiiiliiinn@gmail.com>
2025-12-09 04:27:12 +0800

    Update

--- distillation/src/pages/Home.jsx
+++ distillation/src/pages/Home.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import Loader from '../components/Loader'
 
 const Home = () => {
   return (
-    <section className="w-full h-screen relative bg-black">
-      <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
-        home page
-      </div>
+    <section className="relative w-full h-screen">
+      <script type="module" src="./animate.js"></script>
+      <Canvas
+        className="absolute inset-0 w-full h-full bg-transparent z-0"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader/>}>
+
+        </Suspense>
+      </Canvas>
     </section>
   )
 }
--- distillation/vite.config.js
+++ distillation/vite.config.js
@@ -2,14 +2,21 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    dedupe: ['react', 'react-dom', 'three', '@react-three/fiber', '@react-three/drei']
+  },
+  optimizeDeps: {
+    include: [
+      'react',
+      'react-dom',
+      'three',
+      '@react-three/fiber',
+      '@react-three/drei'
+    ],
+    esbuildOptions: {
+      target: 'esnext'
+    }
+  }
 })
