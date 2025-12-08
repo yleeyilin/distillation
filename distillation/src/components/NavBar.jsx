4ebf0e2947dea85b7ca3e2b8f7a081dea627f2e3
yleeyilin <yiiiliiinn@gmail.com>
2025-12-09 02:32:24 +0800

    Add change

--- distillation/src/components/NavBar.jsx
+++ distillation/src/components/NavBar.jsx
@@ -1,35 +1,43 @@
 import { NavLink } from "react-router-dom";
+import EmailRoundedIcon from "@mui/icons-material/EmailRounded";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 const NavBar = () => {
   return (
     <header className="header flex justify-between items-center px-10 py-5">
-        <NavLink to="/" className="text-4xl rounded-lg items-center justify-center flex font-bold hover:scale-110">
-            <p className="bg-clip-text text-transparent bg-linear-to-r from-purple-700 to-blue-400 font-bold">
+        {/* Logo */}
+        <NavLink to="/" className="text-3xl rounded-lg items-center justify-center flex font-bold">
+            <p className="bg-clip-text text-transparent bg-linear-to-r to-purple-700 from-blue-700 font-bold hover:scale-110">
                 Yi Lin Lee
             </p>
         </NavLink>
-        <nav className="flex text-xl gap-10 font-medium">
+
+        {/* Nav links */}
+        <nav className="flex text-lg gap-10 font-medium border border-blue-800 rounded-full px-10 items-center p-4">
             <NavLink to="/" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
-                <p className="bg-clip-text text-transparent bg-white hover:scale-110">
+                <p className="bg-clip-text text-transparent bg-white hover:scale-110 hover:bg-gray-300 hover:underline">
                     Home
                 </p>
             </NavLink>
             <NavLink to="/about" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
-                <p className="bg-clip-text text-transparent bg-white hover:scale-110">
+                <p className="bg-clip-text text-transparent bg-white hover:scale-110 hover:bg-gray-300 hover:underline">
                     About
                 </p>
             </NavLink>
             <NavLink to="/projects" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
-                <p className="bg-clip-text text-transparent bg-white hover:scale-110">
+                <p className="bg-clip-text text-transparent bg-white hover:scale-110 hover:bg-gray-300 hover:underline">
                     Projects
                 </p>
             </NavLink>
             <NavLink to="/contact" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
-                <p className="bg-clip-text text-transparent bg-white hover:scale-110">
+                <p className="bg-clip-text text-transparent bg-white hover:scale-110 hover:bg-gray-300 hover:underline">
                     Contact
                 </p>
             </NavLink>
         </nav>
+
+        {/* Social icons */}
     </header>
   )
 }
