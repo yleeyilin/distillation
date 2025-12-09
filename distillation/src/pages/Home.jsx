e696f3b71fc7022cfe27bd3d0f3df212be30aab1
yleeyilin <yiiiliiinn@gmail.com>
2025-12-09 18:49:52 +0800

    Add model

--- distillation/src/pages/Home.jsx
+++ distillation/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 import Loader from '../components/Loader'
+import Model from '../models/animate'
 
 const Home = () => {
   return (
@@ -9,12 +11,16 @@ const Home = () => {
         className="absolute inset-0 w-full h-full bg-transparent z-0"
         camera={{ near: 0.1, far: 1000 }}
       >
-        <Suspense fallback={<Loader/>}>
+        <Suspense fallback={<Loader />}>
           <directionalLight />
           <ambientLight />
           <pointLight />
           <spotLight />
           <hemisphereLight />
+
+          <Model scale={0.05}/>
+          
+          <OrbitControls />
         </Suspense>
       </Canvas>
     </section>
--- distillation/vite.config.js
+++ distillation/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  assetsInclude: ['**/*.glb'],
   resolve: {
     dedupe: ['react', 'react-dom', 'three', '@react-three/fiber', '@react-three/drei']
   },
